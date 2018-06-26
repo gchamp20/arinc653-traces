d9a446fdcd52cd30af45289c3f00711ac70a2a4c
Guillaume Champagne <guillaume.champagne@polymtl.ca>
2018-06-26 10:10:34 -0400

    partition ID in stream context. Seems like there's a bug with barectf python bindings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from mos import MOS
 
-timeslices = [10, 15, 20]
+timeslices = [100, 150, 200]
 m = MOS(len(timeslices), timeslices)
 m.run(1000)
--- pos.py
+++ pos.py
@@ -2,13 +2,14 @@ import os
 import time
 import threading
 import sys
-from babeltrace import *
+import babeltrace.writer as btw
+import random
 
 class ClockManager:
     instance = None
     def __init__(self):
         if not ClockManager.instance:
-            ClockManager.instance = CTFWriter.Clock('A_clock')
+            ClockManager.instance = btw.Clock('A_clock')
             ClockManager.instance.description = "Big Clock"
 
     def get_clock(self):
@@ -22,7 +23,7 @@ class WriterManager:
     instance = None
     def __init__(self, trace_path):
         if not WriterManager.instance:
-            WriterManager.instance = CTFWriter.Writer(trace_path)
+            WriterManager.instance = btw.Writer(trace_path)
             WriterManager.instance.add_clock(ClockManager().get_clock())
             WriterManager.instance.add_environment_field("Python_version", str(sys.version_info))
 
@@ -33,26 +34,27 @@ class POSTracer:
     def __init__(self, uuid, trace_path):
         self.c = ClockManager().get_clock()
         self.writer = WriterManager(trace_path).get_writer()
-        self.stream_class = CTFWriter.StreamClass("p" + str(uuid))
+        self.stream_class = btw.StreamClass("p" + str(uuid))
         self.stream_class.clock = self.c
         self.id = uuid
         self.create_event_types()
         self.create_stream()
 
     def create_event_types(self):
-        int32_field_decl = CTFWriter.IntegerFieldDeclaration(32)
-        struct_field_decl = CTFWriter.StructureFieldDeclaration()
+        int32_field_decl = btw.IntegerFieldDeclaration(32)
+        struct_field_decl = btw.StructureFieldDeclaration()
         struct_field_decl.add_field(int32_field_decl, "pid")
         self.stream_class.event_context_type = struct_field_decl
+        self.stream_context_decl = struct_field_decl
 
-        event_class = CTFWriter.EventClass("task_create")
-        int32_type = CTFWriter.IntegerFieldDeclaration(32)
+        event_class = btw.EventClass("task_create")
+        int32_type = btw.IntegerFieldDeclaration(32)
         event_class.add_field(int32_type, "tid")
         self.stream_class.add_event_class(event_class)
         self.tcreateEventClass = event_class
 
-        self.schedswitchEventClass = CTFWriter.EventClass("sched_switch_in")
-        int32_type = CTFWriter.IntegerFieldDeclaration(32)
+        self.schedswitchEventClass = btw.EventClass("sched_switch_in")
+        int32_type = btw.IntegerFieldDeclaration(32)
         self.schedswitchEventClass.add_field(int32_type, "tid")
         self.stream_class.add_event_class(self.schedswitchEventClass)
 
@@ -61,20 +63,25 @@ class POSTracer:
 
     def task_create(self, tid):
         ClockManager().sample()
-        e = CTFWriter.Event(self.tcreateEventClass)
+        e = btw.Event(self.tcreateEventClass)
         e.payload("tid").value = tid
-        print(e)
-        #e.stream_context.field("pid").value = self.id
+
+        ctx = btw.StructureField(self.stream_context_decl)
+        ctx.field("pid").value = self.id
+        e.stream_context = ctx
+
         self.stream.append_event(e)
-        self.flush()
 
     def sched_switch(self, tid):
         ClockManager().sample()
-        e = CTFWriter.Event(self.schedswitchEventClass)
+        e = btw.Event(self.schedswitchEventClass)
         e.payload("tid").value = tid
-        e.stream_event_context.field("pid").value = self.id
+
+        ctx = btw.StructureField(self.stream_context_decl)
+        ctx.field("pid").value = self.id
+        e.stream_context = ctx
+
         self.stream.append_event(e)
-        self.flush()
 
     def flush(self):
         self.stream.flush()
@@ -85,15 +92,30 @@ class POS:
         self.tracer = POSTracer(uuid, os.getcwd() + '/traces')
 
     def run(self, e, done, runtime):
-        print(self.id, "is init")
-        self.tracer.task_create(0)
-        #while True:
+        nb_task = random.randrange(1, 10)
+        print(self.id, "is init", nb_task)
+        tasks = [0 for i in range(nb_task)]
+        t = 0
+        tasks[t] = 1
+
         e.wait()
+        self.tracer.task_create(t + 1)
+        self.tracer.sched_switch(t + 1)
+
         budget = runtime
         while budget > 0:
-            print(self.id, "is running")
+            #print(self.id, "is running")
+            if random.randrange(0, 100) > 80:
+                old_t = t
+                t = random.randrange(0, nb_task)
+                if t != old_t:
+                    if tasks[t] == 0:
+                        self.tracer.task_create(t + 1)
+                        tasks[t] = 1
+                    self.tracer.sched_switch(t + 1)
+
             time.sleep(0.001)
-            self.tracer.sched_switch(0)
+            #self.tracer.sched_switch(0)
             budget -= 1
         self.tracer.flush()
         e.clear()
